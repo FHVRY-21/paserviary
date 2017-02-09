2e398e9ce3d984bf9e2e9fde7077a16632f319bf
Aviary <fahrelaviary@gmail.com>
2017-02-09 07:49:18 +0700

    paserviary

--- resources/views/inputprdks.blade.php
+++ resources/views/inputprdks.blade.php
@@ -154,7 +154,7 @@
           <!-- Sidebar user panel -->
           <div class="user-panel">
             <div class="pull-left image">
-              <img src="/css/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+              <img src="{{ url('/css/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
               <p>{{ \Auth::user()->nama_depan }} {{ \Auth::user()->nama_belakang }}</p>
